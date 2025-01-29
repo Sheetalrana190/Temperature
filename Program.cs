d4d0aebf5f2f244243c5c47c52d5f7c2ec057021
Sheetal <sheetalrana190@gmail.com>
2025-01-29 16:30:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Test
+{
+    class ExampleClass
+    {
+        static void Main()
+        {
+            double f = 0, c = 0;
+            Console.WriteLine("Enter temperature in Fahrenheit");
+            f = Convert.ToDouble(Console.ReadLine());
+            
+            c = (f - 32) * 5 / 9; 
+            Console.WriteLine("\nTemperature in Celsius " + c);
+            Console.WriteLine("\nEnter temperature in Celsius");
+            c = Convert.ToDouble(Console.ReadLine());
+            f = (c * 9) / 5 + 32; 
+            Console.WriteLine("\nTemperature in Fahrenheit  " + f);
+        }
+    }
+}
